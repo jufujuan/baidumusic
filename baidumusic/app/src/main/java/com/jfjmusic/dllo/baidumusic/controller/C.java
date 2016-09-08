4698cb7334517f34ca73eea5e9f3e728a375b9ba
jfj <2376562567@qq.com>
2016-09-08 19:29:29 +0800

    一级分包以及占位类

---
+++ baidumusic/app/src/main/java/com/jfjmusic/dllo/baidumusic/controller/C.java
@@ -0,0 +1,7 @@
+package com.jfjmusic.dllo.baidumusic.controller;
+
+/**
+ * Created by dllo on 16/9/8.
+ */
+public class C {
+}
---
+++ baidumusic/app/src/main/java/com/jfjmusic/dllo/baidumusic/utils/U.java
@@ -0,0 +1,7 @@
+package com.jfjmusic.dllo.baidumusic.utils;
+
+/**
+ * Created by dllo on 16/9/8.
+ */
+public class U {
+}
