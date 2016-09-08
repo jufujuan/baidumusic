2023d33b9b8c1b95790d7fceadb8d611cfe2ee31
jfj <2376562567@qq.com>
2016-09-08 19:42:06 +0800

    model包的的分类以及删除controller包的占位类C

--- baidumusic/app/src/main/java/com/jfjmusic/dllo/baidumusic/model/M.java
+++
@@ -1,7 +0,0 @@
-package com.jfjmusic.dllo.baidumusic.model;
-
-/**
- * Created by dllo on 16/9/8.
- */
-public class M {
-}
---
+++ baidumusic/app/src/main/java/com/jfjmusic/dllo/baidumusic/model/db/Db.java
@@ -0,0 +1,7 @@
+package com.jfjmusic.dllo.baidumusic.model.db;
+
+/**
+ * Created by dllo on 16/9/8.
+ */
+public class Db {
+}
