8256b227c85f65c5c1a6765190c7f76e326f7dce
jfj <2376562567@qq.com>
2016-09-08 19:49:04 +0800

    修改MainActivity的位置，删除Activity的占位类

--- baidumusic/app/src/main/java/com/jfjmusic/dllo/baidumusic/controller/activity/MainActivity.java
+++ baidumusic/app/src/main/java/com/jfjmusic/dllo/baidumusic/controller/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.jfjmusic.dllo.baidumusic;
+package com.jfjmusic.dllo.baidumusic.controller.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.jfjmusic.dllo.baidumusic.R;
 
 public class MainActivity extends AppCompatActivity {
 
