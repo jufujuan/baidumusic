c81651377fc7992b9500ff15ab9dbb584a439f5c
jfj <2376562567@qq.com>
2016-09-08 19:55:00 +0800

    创建Activity的基类

---
+++ baidumusic/app/src/main/java/com/jfjmusic/dllo/baidumusic/controller/activity/AbsBaseActivity.java
@@ -0,0 +1,18 @@
+package com.jfjmusic.dllo.baidumusic.controller.activity;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by dllo on 16/9/8.
+ * Activity的基类
+ */
+public class AbsBaseActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
