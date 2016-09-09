11117a361b38c36f999979c321955afccc4ad573
jfj <2376562567@qq.com>
2016-09-09 22:07:12 +0800

    fragment_mine界面的搭建

--- baidumusic/app/src/main/java/com/jfjmusic/dllo/baidumusic/controller/activity/MainActivity.java
+++ baidumusic/app/src/main/java/com/jfjmusic/dllo/baidumusic/controller/activity/MainActivity.java
@@ -1,16 +1,11 @@
 package com.jfjmusic.dllo.baidumusic.controller.activity;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.TextView;
+
 
 import com.jfjmusic.dllo.baidumusic.R;
-import com.jfjmusic.dllo.baidumusic.controller.fragment.TestA;
 
 public class MainActivity extends AbsBaseActivity{
 
-    private TextView textView;
 
     @Override
     protected int setLayout() {
@@ -20,16 +15,9 @@ public class MainActivity extends AbsBaseActivity{
 
     @Override
     protected void initViews() {
-         textView=byView(R.id.textView);
     }
 
     @Override
     protected void initDatas() {
-         textView.setOnClickListener(new View.OnClickListener() {
-             @Override
-             public void onClick(View v) {
-                 goTo(MainActivity.this, TestB.class);
-             }
-         });
     }
 }
